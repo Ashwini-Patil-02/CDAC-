dc1a4b8d19585c0d23225ccab1cae76f747072e6
Ashwini-Patil-02 <129053501+Ashwini-Patil-02@users.noreply.github.com>
2024-08-24 00:21:27 +0530

    Add files via upload
    Assignment1 uploaded

---
+++ Logic Building/Assignments/Assignment1/Areaofrectangle.java
@@ -0,0 +1,8 @@
+public class Areaofrectangle {
+    public static void main(String[] args) {
+        int length = 8;
+        int width = 4;
+        int area = length * width;
+        System.out.println("The area of the rectangle is: " + area);
+    }
+}
