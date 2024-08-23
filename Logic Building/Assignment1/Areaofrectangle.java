79a5d553fd041197d6c95bf932e5d5f79f24d023
Ashwini-Patil-02 <129053501+Ashwini-Patil-02@users.noreply.github.com>
2024-08-24 00:12:10 +0530

    Delete Logic Building/Assignment1 directory

--- Logic Building/Assignment1/Areaofrectangle.java
+++
@@ -1,8 +0,0 @@
-public class Areaofrectangle {
-    public static void main(String[] args) {
-        int length = 8;
-        int width = 4;
-        int area = length * width;
-        System.out.println("The area of the rectangle is: " + area);
-    }
-}
